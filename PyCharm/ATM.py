f447c9ac5533f8f0f9ace6f30a40753ca46b9561
Md Sohel Rana <rana.sohel0038@gmail.com>
2026-09-22 12:27:12 +0600

    Merge remote-tracking branch 'origin/main'

--- PyCharm/ATM.py
+++ PyCharm/ATM.py
@@ -6,10 +6,15 @@ input("Please insert your card and press Enter once inserted>>")
 
 correct_pin = 1234
 balance = 5000
+while True:
+    pin = int(input("Enter your PIN:"))
+    if pin == correct_pin:
+        print("PIN verified successfully")
+        break
+    else:
+        print("Incorrect PIN! Please insert correct PIN")
 
-pin=int(input("Enter your pin:"))
-if pin == correct_pin:
- while True:
+while True:
     print("\nMain MENU>")
     print("1. Check Balance")
     print("2. Withdraw Cash")
@@ -23,23 +28,23 @@ if pin == correct_pin:
 
     if option == 1:
         print("Your Balance is:",balance)
-        input("\nPress Enter to main menu")
+        input("\nPress Enter to main menu>>")
     elif option == 2:
         amount = int(input("Enter withdrawal amount:"))
         if amount <= balance:
             balance = balance - amount
             print("Withdrawal successful")
             print("Your remaining Balance is:",balance)
-            input("\nPress Enter to main menu")
+            input("\nPress Enter to main menu>>")
         else:
             print("Insufficient funds")
-            input("\nPress Enter to main menu")
+            input("\nPress Enter to main menu>>")
     elif option == 3:
         amount = int(input("Enter deposit amount:"))
         balance = balance + amount
         print("Deposit successful")
         print("Your Balance is:",balance)
-        input("\nPress Enter to main menu")
+        input("\nPress Enter to main menu>>")
     elif option == 4:
         account_no = input("Enter recipient account number:")
         transfer_amount = int(input("Enter transfer amount:"))
@@ -47,10 +52,10 @@ if pin == correct_pin:
             balance = balance-transfer_amount
             print(f"Transfer successful {transfer_amount} to Account: {account_no}")
             print("Your Your Balance is:",balance)
-            input("\nPress Enter to main menu")
+            input("\nPress Enter to main menu>>")
         else:
             print("Insufficient funds for transfer")
-            input("\nPress Enter to main menu")
+            input("\nPress Enter to main menu>>")
     elif option == 5:
         current_pin = int(input("Enter your current PIN:"))
         if current_pin == correct_pin:
@@ -59,20 +64,20 @@ if pin == correct_pin:
             if new_pin == confirm_pin:
                 confirm_pin = new_pin
                 print("PIN successfully changed")
-                input("\nPress Enter to main menu")
+                input("\nPress Enter to main menu>>")
             else:
                 print("New PIN and Confirm PIN are different")
-                input("\nPress Enter to main menu")
+                input("\nPress Enter to main menu>>")
         else:
             print("Incorrect current PIN")
-            input("\nPress Enter to main menu")
+            input("\nPress Enter to main menu>>")
     elif option == 6:
         print("\n--- HELP & SUPPORT ---")
         print("1. Helpline: 16216 / +8809666716216")
         print("2. Daily withdrawal limit: 20000")
         print("3. For card swallowed or transaction issues, contact branch manager immediately")
         print("4. Email: support@ranasatm.com")
-        input("\nPress Enter to main menu")
+        input("\nPress Enter to main menu>>")
 
     elif option == 7:
         print("Please collect your card")
@@ -80,6 +85,4 @@ if pin == correct_pin:
         break
     else:
      print("Invalid option")
-     input("\nPress Enter to main menu")
-else:
-    print("Wrong Pin")
+     input("\nPress Enter to main menu>>")
