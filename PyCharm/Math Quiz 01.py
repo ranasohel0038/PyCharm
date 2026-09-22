c198f8695bfa0ee08c84b130c986e488ee764377
Md Sohel Rana <rana.sohel0038@gmail.com>
2026-09-22 17:09:52 +0600

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	PyCharm/ATM.py

---
+++ PyCharm/Math Quiz 01.py
@@ -0,0 +1,13 @@
+x=10
+if x>5:
+    print("A")
+    if x>20:
+        print("B")
+    else:
+        print("C")
+else:
+    print("D")
+if x==10:
+    print("E")
+else:
+    print("F")
---
+++ PyCharm/Math Quiz 02.py
@@ -0,0 +1,5 @@
+x=10
+if x !=5:
+    print("Hello")
+else:
+    print("World")
